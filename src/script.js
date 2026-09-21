0c7e3f2d7699f8214d2a8162a855be59a6bf4e0d
thaylla20 <luziaquirino717@gmail.com>
2026-09-21 16:17:31 -0300

    Cria tela de login

--- src/script.js
+++ src/script.js
@@ -1,7 +1,34 @@
 function entrar() {
-    alert("Bem-vindo ao ConectaMente! 🧠");
+    document.querySelector("main").innerHTML = `
+        <h2>🔐 Entrar</h2>
+
+        <p>Entre na sua conta do ConectaMente.</p>
+
+        <input type="email" id="email" placeholder="Seu e-mail">
+
+        <input type="password" id="senha" placeholder="Sua senha">
+
+        <button onclick="fazerLogin()">Entrar</button>
+
+        <button onclick="voltarInicio()">Voltar</button>
+    `;
 }
 
 function criarConta() {
-    alert("Vamos criar sua conta!");
+    alert("A tela de cadastro será criada em breve!");
+}
+
+function fazerLogin() {
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    if (email === "" || senha === "") {
+        alert("Preencha seu e-mail e sua senha.");
+    } else {
+        alert("Login realizado! 🧠");
+    }
+}
+
+function voltarInicio() {
+    location.reload();
 }
